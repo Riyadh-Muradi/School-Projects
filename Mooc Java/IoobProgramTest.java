b7993b28c87028edca6b73f58650ebdb67564cae
Riyadh Muradi <theahtic@gmail.com>
2023-08-08 10:17:58 +0300

    Update IoobProgramTest.java

--- Mooc Java/IoobProgramTest.java
+++ Mooc Java/IoobProgramTest.java
@@ -1,4 +1,3 @@
-
 import fi.helsinki.cs.tmc.edutestutils.Points;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -6,12 +5,18 @@ import org.junit.Test;
 @Points("03-03")
 public class IoobProgramTest {
 
+    // Test method to check if the program causes an IndexOutOfBoundsException
     @Test
     public void causingIndexOutOfBoundsException() {
         try {
+            // Call the main method of IoobProgram class
             IoobProgram.main(new String[]{});
+            
+            // If the program execution does not throw an exception, fail the test
             fail("Execution of the program should cause an IndexOutOfBoundsException. Now it didn't happen.");
         } catch (IndexOutOfBoundsException e) {
+            // If an IndexOutOfBoundsException is caught, the test is successful
+            // No action needed as the exception is expected
         }
     }
 }
